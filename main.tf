bd244cea6baa5ed5aaa528ebbed03affebbf2740
devank silswal <devanksilswal@devanks-MacBook-Air.local>
2026-08-24 23:46:22 +0530

    test: bad terraform formatting

--- main.tf
+++ main.tf
@@ -3,12 +3,31 @@ terraform {
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-     version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
   region = "ap-south-1"
+}
+
+resource "aws_security_group" "test_insecure" {
+  name        = "test-insecure-sg"
+  description = "Security group for tfsec testing"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
